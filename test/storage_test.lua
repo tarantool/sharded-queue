8faee83f88f78072dac0809d517cee1297cd9f40
Oleg Jukovec <oleg.jukovec@tarantool.org>
2023-06-14 13:53:12 +0300

    internal: remove unused log variable

--- test/storage_test.lua
+++ test/storage_test.lua
@@ -1,7 +1,6 @@
 #!/usr/bin/env tarantool
 
 local t = require('luatest')
-local log = require('log')  -- luacheck: ignore
 local g = t.group('storage')
 
 local storage = require('sharded_queue.storage')
