960376e3e040c3c266f6cdf9a013b8feb1348931
Oleg Jukovec <oleg.jukovec@tarantool.org>
2024-04-03 11:36:48 +0300

    internal: split api.lua to logical blocks
    The patch splits a large file `api.lua` with a Cartridge
    role for a few smaller files based on their functionality.
    It will help to create a Tarantool 3.0 role from that blocks
    instead of copy-paste.
    
    Part of #68

---
+++ sharded_queue/router/metrics.lua
@@ -0,0 +1,31 @@
+local metrics = require('sharded_queue.metrics')
+local stash = require('sharded_queue.stash')
+local stats_storage = require('sharded_queue.stats.storage')
+
+local stash_names = {
+    metrics_stats = '__sharded_queue_router_metrics_stats',
+}
+stash.setup(stash_names)
+
+local metrics_stats = metrics.init(stash.get(stash_names.metrics_stats))
+
+local function enable(queue)
+    local get_statistic = function(tube)
+        return queue.statistics(tube)
+    end
+    metrics_stats:enable('api', queue.map(), get_statistic)
+end
+
+local function observe(latency, tube, method, ok)
+    metrics_stats:observe(latency, tube, method, ok)
+end
+
+local function disable()
+    metrics_stats:disable()
+end
+
+return {
+    enable = enable,
+    observe = observe,
+    disable = disable,
+}
